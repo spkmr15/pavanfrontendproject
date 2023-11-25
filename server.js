f35b118ba89b67fce13462c3a677156308c2b6c2
PAVANKUMAR.S <114171301+spkmr15@users.noreply.github.com>
2023-11-25 10:20:24 +0530

    Add files via upload
    cart checkout files

---
+++ server.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import Stripe from 'stripe';
+
+dotenv.config();
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html", {root: "public"});
+});
+app.get("/success", (req, res) => {
+    res.sendFile("success.html", {root: "public"});
+});
+
+app.get("/cancel", (req, res) => {
+    res.sendFile("cancel.html", {root: "public"});
+});
+
+
+let stripeGateway = Stripe(process.env.stripe_key);
+let DOMAIN = process.env.DOMAIN;
+app.post("/stripe-checkout", async (req, res) => {
+    const lineItems = req.body.items.map((item) => {
+        const unitAmount = parseInt(item.price.replace(/[^0-9.-]+/g, "") * 100);
+        console.log("item-price:", item.price);
+        console.log("unitAmount:", unitAmount);
+        return {
+            price_data: {
+                currency: "usd", 
+                product_data: {
+                    name: item.title,
+                    images: [item.productImg],
+                },
+                unit_amount: unitAmount, 
+            },
+            quantitty: item.quantitty,
+        };
+    });
+    console.log("lineItems:", lineItems);
+    const session = await stripeGateway.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        success_url: `${DOMAIN}/success`,
+        cancel_url: `${DOMAIN}/cancel`,
+        line_items: lineItems,
+        billing_address_collection: "required"
+    });
+    res.json(session.url);
+});
+
+
+
+app.listen(3000, () => {
+    console.log("listening on port 3000; ");
+});
